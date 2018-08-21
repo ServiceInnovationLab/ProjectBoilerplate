6e537ac9254d70a2a169a38b8fcf7d8db2e8b5ee
Awesome Code <team@xinminlabs.com>
2018-08-21 17:37:42 +0000

    Auto corrected by following Javascript implicit-arrow-linebreak

--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from '../components/App';
 
-const AppRouter = () => 
-  <BrowserRouter>
+const AppRouter = () => <BrowserRouter>
     <App />
   </BrowserRouter>;
 export default AppRouter;
